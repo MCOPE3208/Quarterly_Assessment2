05f85776f22a9fce503dd437629ee4928c782431
Makenzie Cope <mcope3208@gmail.com>
2024-03-05 16:35:46 -0600

    Create database and add in a table for tax questions and answers

--- z1_create.py
+++ z1_create.py
@@ -2,9 +2,15 @@ import sqlite3
 conn = sqlite3.connect('Quizbowl.db')
 cr = conn.cursor()
 
-cr.execute("""
-           CREATE TABLE IF NOT EXISTS QuestAns (
-           id INTEGER PRIMARY KEY,
-           question TEXT,
-           answer TEXT)
-           """)
+# Create the 'Taxation' table if it doesn't exist
+cr.execute('''
+    CREATE TABLE IF NOT EXISTS Taxation (
+        id INTEGER PRIMARY KEY,
+        question TEXT,
+        option1 TEXT,
+        option2 TEXT,
+        option3 TEXT,
+        option4 TEXT,
+        correct_answer INTEGER
+    );
+''')
