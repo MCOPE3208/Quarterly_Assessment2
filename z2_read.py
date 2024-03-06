0768b0e10c9793b60bd252155e2baf7f848dddc1
Makenzie Cope <mcope3208@gmail.com>
2024-03-06 10:25:26 -0600

    Made the print statements more readable when you call the table/column names and questions it is spaced out and easier to read

--- z2_read.py
+++ z2_read.py
@@ -3,16 +3,16 @@ conn = sqlite3.connect('Quizbowl.db')
 cr = conn.cursor()
 
 cr.execute("SELECT name FROM sqlite_master WHERE  type='table';")
-print(cr.fetchall())
+print("Table names from database file:\n",cr.fetchall(),"\n")
 
 cr.execute("PRAGMA table_info({});".format("Taxation"))
 columns = cr.fetchall()
 
 if columns:
     column_names = [column[1] for column in columns]
-    print(f"Column Names: {', '.join(column_names)}")
+    print(f"Column Names: {', '.join(column_names)}","\n")
 else:
     print(f"No columns found in the table {"Taxation"}.")
 
 cr.execute("SELECT * FROM Taxation")
-print(cr.fetchall())
+print("Questions from Taxation table:\n",cr.fetchall(),"\n")
