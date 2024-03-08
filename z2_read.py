e8e74b2849e5264a70dc89b34b07946cab68b1ad
Makenzie Cope <mcope3208@gmail.com>
2024-03-08 12:43:55 -0600

    Pull column names from all tables and questions from a certain table

--- z2_read.py
+++ z2_read.py
@@ -5,3 +5,15 @@ cr = conn.cursor()
 cr.execute("SELECT name FROM sqlite_master WHERE  type='table';")
 print("Table names from database file:\n",cr.fetchall(),"\n")
 
+cr.execute("PRAGMA table_info({});".format("Taxation"))
+columns = cr.fetchall()
+
+if columns:
+    column_names = [column[1] for column in columns]
+    print(f"Column Names: {', '.join(column_names)}","\n")
+else:
+    print(f"No columns found in the table {"Taxation"}.")
+
+#to view the questions in a specific table put the name in after the word FROM in the next line of code
+cr.execute("SELECT * FROM BusinessAppdev")
+print("Questions from table:\n",cr.fetchall(),"\n")
