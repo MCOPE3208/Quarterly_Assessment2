c59bb483fba97122c4fd1f6479873703558e16a5
Makenzie Cope <mcope3208@gmail.com>
2024-03-08 12:43:11 -0600

    Create a way to pull the table names from database

--- z2_read.py
+++ z2_read.py
@@ -5,14 +5,3 @@ cr = conn.cursor()
 cr.execute("SELECT name FROM sqlite_master WHERE  type='table';")
 print("Table names from database file:\n",cr.fetchall(),"\n")
 
-cr.execute("PRAGMA table_info({});".format("Taxation"))
-columns = cr.fetchall()
-
-if columns:
-    column_names = [column[1] for column in columns]
-    print(f"Column Names: {', '.join(column_names)}","\n")
-else:
-    print(f"No columns found in the table {"Taxation"}.")
-
-cr.execute("SELECT * FROM Taxation")
-print("Questions from Taxation table:\n",cr.fetchall(),"\n")
